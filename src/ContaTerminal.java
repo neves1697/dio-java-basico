e269069cc343bc8921e66eabce4c969128c63882
Anderson Neves <neves1697@gmail.com>
2024-06-26 20:21:27 -0300

    Finalizando desafio

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+        /*
+         * TODO
+         * Conhecer e importar a classe Scanner
+         * Exibir as mensagens para o usuário
+         * Obter pelo Scanner os valores digitados
+         * Exibir a mensagem criada
+         */
+
+        int numero = 0;
+        String agencia = "", nomeCliente = "";
+        double saldo = 0;
+
+        System.out.println("Por favor, digite a unidade da sua Agência !");
+        numero = sc.nextInt();
+
+        System.out.println("Digite a Agência\n");
+        agencia = sc.next();
+
+        System.out.println("Digite o nome do cliente\n");
+        nomeCliente = sc.next();
+
+        System.out.println("Digite o saldo\n");
+        saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + " , obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + " , conta "
+                + numero + " e seu saldo " + saldo + " já está disponível para saque! \n");
     }
 }
